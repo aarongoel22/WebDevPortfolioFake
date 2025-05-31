6b66654978c4924638c43524cb22f232e1c35ee3
aarongoel22 <160720232+aarongoel22@users.noreply.github.com>
2025-05-31 22:38:00 +1200

    Commit V3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = ({menuOpen, setMenuOpen}) => {
 
         <div className='max-w-5xl mx-auto px-4'>
             <div className='flex justify-between items-center h-16'>
-                <a href="#home" className='font-mono text-xl font-bold text-white'> John Doe <span className='text-blue-500'>Portfolio</span>  
+                <a href="#home" className='font-mono text-xl font-bold text-white'><span className='text-blue-500'>Portfolio</span>  
                 </a>
 
                 <div className='w-7 h-5 relative cursor-pointer z-40 md:hidden' onClick={() => setMenuOpen((prev) => !prev)}>
